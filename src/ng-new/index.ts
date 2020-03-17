d63e4fe1fd08043e449a49063db8d9d702930cdb
Wykks <contact@wykks.eu>
2020-03-17 16:07:21 +0100

    Remove remaining e2e config & actually add vscode config

--- src/ng-new/index.ts
+++ src/ng-new/index.ts
@@ -1,4 +1,4 @@
-import { strings, workspaces } from '@angular-devkit/core';
+import { strings, workspaces, JsonArray } from "@angular-devkit/core";
 import {
   apply,
   applyToSubtree,
@@ -10,99 +10,114 @@ import {
   template,
   Tree,
   url
-} from '@angular-devkit/schematics';
-import { NodePackageInstallTask, RepositoryInitializerTask } from '@angular-devkit/schematics/tasks';
-import { addPackageJsonDependency, removePackageJsonDependency } from '@schematics/angular/utility/dependencies';
-import { updateWorkspace } from '@schematics/angular/utility/workspace';
-import { addOnPushToComponent } from './addOnPush';
-import { baseDependencies } from './dependencies';
-import { jestChain } from './jest';
+} from "@angular-devkit/schematics";
+import {
+  NodePackageInstallTask,
+  RepositoryInitializerTask
+} from "@angular-devkit/schematics/tasks";
+import {
+  addPackageJsonDependency,
+  removePackageJsonDependency
+} from "@schematics/angular/utility/dependencies";
+import { updateWorkspace } from "@schematics/angular/utility/workspace";
+import { addOnPushToComponent } from "./addOnPush";
+import { baseDependencies } from "./dependencies";
+import { jestChain } from "./jest";
 
 function removeProtractor(host: Tree) {
-  host.getDir('e2e').visit(file => {
+  host.getDir("e2e").visit(file => {
     host.delete(file);
   });
-  removePackageJsonDependency(host, 'protractor');
+  removePackageJsonDependency(host, "protractor");
   return host;
 }
 
 function editTsConfig(host: Tree) {
-  const tsConfigPath = '/tsconfig.json';
+  const tsConfigPath = "/tsconfig.json";
 
-  const tsConfigText = host.read(tsConfigPath)!.toString('utf-8');
+  const tsConfigText = host.read(tsConfigPath)!.toString("utf-8");
   const tsConfig = JSON.parse(tsConfigText);
 
   tsConfig.compilerOptions.strict = true;
   tsConfig.compilerOptions.strictPropertyInitialization = false;
   tsConfig.compilerOptions.noUnusedParameters = true;
   tsConfig.compilerOptions.noUnusedLocals = true;
-  tsConfig.compilerOptions.plugins = [{ name: 'typescript-tslint-plugin' }];
+  tsConfig.compilerOptions.plugins = [{ name: "typescript-tslint-plugin" }];
   tsConfig.angularCompilerOptions.strictTemplates = true;
 
   host.overwrite(tsConfigPath, JSON.stringify(tsConfig, null, 2));
 }
 
 function addConfigToPackageJSON(host: Tree) {
-  const packageJsonPath = '/package.json';
+  const packageJsonPath = "/package.json";
 
-  const packageJsonText = host.read(packageJsonPath)!.toString('utf-8');
+  const packageJsonText = host.read(packageJsonPath)!.toString("utf-8");
   const packageJson = JSON.parse(packageJsonText);
 
   packageJson.husky = {
     hooks: {
-      'pre-commit': 'lint-staged'
+      "pre-commit": "lint-staged"
     }
   };
 
-  packageJson['lint-staged'] = {
-    '*.{js,json,css,scss,md,ts,html}': 'prettier --write',
-    '*.ts': 'tslint -c tslint.json -p tsconfig.json --fix'
+  packageJson["lint-staged"] = {
+    "*.{js,json,css,scss,md,ts,html}": "prettier --write",
+    "*.ts": "tslint -c tslint.json -p tsconfig.json --fix"
   };
 
   packageJson.commitizen = {
-    path: 'cz-conventional-changelog'
+    path: "cz-conventional-changelog"
   };
 
   const scripts = packageJson.scripts;
 
-  delete scripts['e2e'];
+  delete scripts["e2e"];
 
-  scripts.test = 'ng test --watch';
-  scripts.build = 'ng build --prod --no-progress';
-  scripts['test:ci'] = 'ng test --ci --silent';
-  scripts.commit = 'lint-staged && git-cz --no-verify';
+  scripts.test = "ng test --watch";
+  scripts.build = "ng build --prod --no-progress";
+  scripts["test:ci"] = "ng test --ci --silent";
+  scripts.commit = "lint-staged && git-cz --no-verify";
 
   host.overwrite(packageJsonPath, JSON.stringify(packageJson, null, 2));
 }
 
 function addDependenciesToPackageJson(host: Tree) {
-  baseDependencies.forEach(dependency => addPackageJsonDependency(host, dependency));
+  baseDependencies.forEach(dependency =>
+    addPackageJsonDependency(host, dependency)
+  );
   return host;
 }
 
 function editAngularConfig(workspace: workspaces.WorkspaceDefinition) {
   workspace.projects.forEach(project => {
-    project.targets.delete('e2e');
-    project.targets.get('lint')!.options!.format = 'stylish';
+    project.targets.delete("e2e");
+    const lintTargetOptions = project.targets.get("lint")!.options!;
+    lintTargetOptions.format = "stylish";
+    const e2eConfigIdx = (<JsonArray>(
+      lintTargetOptions.tsConfig
+    )).findIndex(config => /e2e/.test(<string>config));
+    if (e2eConfigIdx > -1) {
+      (<JsonArray>lintTargetOptions.tsConfig).splice(e2eConfigIdx, 1);
+    }
   });
 }
 
 function editTsLint(host: Tree) {
-  const tsLintPath = '/tslint.json';
+  const tsLintPath = "/tslint.json";
 
-  const tsLintText = host.read(tsLintPath)!.toString('utf-8');
+  const tsLintText = host.read(tsLintPath)!.toString("utf-8");
   const tsLint = JSON.parse(tsLintText);
 
   if (tsLint.extends) {
     if (Array.isArray(tsLint.extends)) {
-      tsLint.extends.push('tslint-config-prettier');
+      tsLint.extends.push("tslint-config-prettier");
     } else {
-      tsLint.extends = [tsLint.extends, 'tslint-config-prettier'];
+      tsLint.extends = [tsLint.extends, "tslint-config-prettier"];
     }
   } else {
-    tsLint.extends = ['tslint-config-prettier'];
+    tsLint.extends = ["tslint-config-prettier"];
   }
-  tsLint.rules['prefer-on-push-component-change-detection'] = true;
+  tsLint.rules["prefer-on-push-component-change-detection"] = true;
 
   host.overwrite(tsLintPath, JSON.stringify(tsLint, null, 2));
 }
@@ -118,20 +133,20 @@ export default function(options: any): Rule {
     newProjectRoot: options.newProjectRoot,
     strict: false,
     packageManager: options.packageManager,
-    projectRoot: '',
+    projectRoot: "",
     inlineStyle: options.inlineStyle,
     inlineTemplate: options.inlineTemplate,
     prefix: options.prefix,
     viewEncapsulation: options.viewEncapsulation,
     routing: options.routing,
-    style: 'scss',
+    style: "scss",
     skipInstall: true,
     skipGit: true,
     skipTests: true
   };
 
   return chain([
-    externalSchematic('@schematics/angular', 'ng-new', angularNgNewOptions),
+    externalSchematic("@schematics/angular", "ng-new", angularNgNewOptions),
     applyToSubtree(options.directory, [
       jestChain,
       removeProtractor,
@@ -141,7 +156,7 @@ export default function(options: any): Rule {
       addConfigToPackageJSON,
       editTsLint,
       addOnPushToComponent,
-      mergeWith(apply(url('./files'), [template({ utils: strings })]))
+      mergeWith(apply(url("./files"), [template({ utils: strings })]))
     ]),
     (_host: Tree, context: SchematicContext) => {
       let packageTask;
@@ -154,9 +169,17 @@ export default function(options: any): Rule {
         );
       }
       if (!options.skipGit) {
-        const commit = typeof options.commit == 'object' ? options.commit : !!options.commit ? {} : false;
+        const commit =
+          typeof options.commit == "object"
+            ? options.commit
+            : !!options.commit
+            ? {}
+            : false;
 
-        context.addTask(new RepositoryInitializerTask(options.directory, commit), packageTask ? [packageTask] : []);
+        context.addTask(
+          new RepositoryInitializerTask(options.directory, commit),
+          packageTask ? [packageTask] : []
+        );
       }
     }
   ]);
